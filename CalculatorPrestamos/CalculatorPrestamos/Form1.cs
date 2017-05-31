ec8ee4c0a0f247861c521db2f0eec412e5670a9b
guzmai11 <guzmai11@TASANI-GOMEZRO>
2017-05-31 10:27:36 -0400

    Codigo y Diseño

---
+++ CalculatorPrestamos/CalculatorPrestamos/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorPrestamos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double capital, interes, meses, Resultado;
+            string cadena = "";
+            capital = Convert.ToDouble(textBox1.Text);
+            interes = Convert.ToDouble(textBox2.Text);
+            meses = Convert.ToDouble(textBox3.Text);
+
+
+            for (int i = 1; i <= meses; i++)
+            {
+
+                Resultado = (capital * (1 + (interes / 100) * i));
+                cadena += (i + "\t" + string.Format("{0:C}", Resultado + "\r\n"));
+            }
+            txtResult.Text = cadena;
+        }
+    }
+}
